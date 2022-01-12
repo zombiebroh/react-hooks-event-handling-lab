760c5e9020fcd0f0e861b47dcfbb8810b51484d9
Gehrig Barnes <gehrigbarnes@gmail.com>
2022-01-12 06:55:41 -0600

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe (){
+    function handleFocus() {
+        console.log('Good!')
+    }
+    function handleBlur() {
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>
+            Eyes on me
+        </button>
+    )
+}
+
+export default EyesOnMe;
